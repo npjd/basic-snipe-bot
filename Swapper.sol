3634a91d7e8b14c72ae3775590ae42e1938dd24d
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-08-20 20:01:04 -0400

    imported modules and interfaces

---
+++ Swapper.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+
+interface IUniswapV2Router {
+    function getAmountsOut(uint256 amountIn, address[] memory path)
+        external
+        view
+        returns (uint256[] memory amounts);
+
+    function swapExactTokensForTokens(
+        //amount of tokens we are sending in
+        uint256 amountIn,
+        //the minimum amount of tokens we want out of the trade
+        uint256 amountOutMin,
+        //list of token addresses we are going to trade in.  this is necessary to calculate amounts
+        address[] calldata path,
+        //this is the address we are going to send the output tokens to
+        address to,
+        //the last time that the trade is valid for
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+interface IUniswapV2Pair {
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+}
+
+interface IUniswapV2Factory {
+    function getPair(address token0, address token1) external returns (address);
+}
