fb80d62e0816972e9b69142701ec2ca58b41212c
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-08-27 22:08:39 -0400

    created basic contract

--- Swapper.sol
+++ Swapper.sol
@@ -31,7 +31,6 @@ interface IERC20 {
     );
 }
 
-
 interface IUniswapV2Router {
     function getAmountsOut(uint256 amountIn, address[] memory path)
         external
@@ -67,4 +66,66 @@ interface IUniswapV2Pair {
 
 interface IUniswapV2Factory {
     function getPair(address token0, address token1) external returns (address);
+}
+
+contract Swapper {
+    address private constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    // basic function for swapping tokens using uniswap v2
+    function swap(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _to
+    ) external {
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
+
+        address[] memory path;
+
+        if (_tokenIn == WETH || _tokenOut == WETH) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WETH;
+            path[2] = _tokenOut;
+        }
+
+        IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            _to,
+            block.timestamp
+        );
+    }
+
+    // Function that lets you know minimum amount of tokens you can get out of the swap
+    function getAmountOutMin(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn
+    ) external view returns (uint256) {
+        address[] memory path;
+        if (_tokenIn == WETH || _tokenOut == WETH) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WETH;
+            path[2] = _tokenOut;
+        }
+
+        uint256[] memory amountOutMins = IUniswapV2Router(UNISWAP_V2_ROUTER)
+            .getAmountsOut(_amountIn, path);
+        return amountOutMins[path.length - 1];
+    }
 }
